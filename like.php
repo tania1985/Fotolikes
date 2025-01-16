d47fbe19b43ff6f7ef6a9e39bcdfd501718f164c
tania1985 <testevezbautista@danielcastelao.org>
2025-01-16 20:07:46 +0100

    dando estilos

--- like.php
+++ like.php
@@ -5,14 +5,18 @@ if (!isset($_SESSION["idusuario"])) {
     exit();
 }
 if (isset($_GET["idfoto"])) {
-    include("conexiondb.php");
-    $sql = "INSERT INTO likes (idfoto, idusuario) VALUES (:idfoto, :idusuario)";
-    $stm = $conexion->prepare($sql);
-    $stm->bindParam(":idfoto", $_GET["idfoto"]);
-    $stm->bindParam(":idusuario", $_SESSION["idusuario"]);
-    $stm->execute();
-    header("Location: index.php");
-    exit();
+    try {
+        include("conexiondb.php");
+        $sql = "INSERT INTO likes (idfoto, idusuario) VALUES (:idfoto, :idusuario)";
+        $stm = $conexion->prepare($sql);
+        $stm->bindParam(":idfoto", $_GET["idfoto"]);
+        $stm->bindParam(":idusuario", $_SESSION["idusuario"]);
+        $stm->execute();
+        header("Location: index.php");
+        exit();
+    } catch (Exception $e) {
+        echo "Error al dar like".$e->getMessage();
+        exit();
+    }
 }
-
 ?>
